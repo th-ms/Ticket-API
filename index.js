d6ef2326cfba5c4756c62422bc94154c20a981a5
Thomas Payton <tpayton@themail.cc>
2020-08-26 17:23:52 -0500

    Wow

--- index.js
+++ index.js
@@ -33,6 +33,19 @@ console.log('Connected to DB'))
 //         console.log("Error fetching ticket.js")
 //         console.log(error)
 //     })
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
 app.use(allowCrossDomain)
 app.use(express.static('public'));
 app.use(bodyParser.json());
