1683d48a4edd9444750fbd8d36d21c15d19d572c
Thomas Payton <tpayton@themail.cc>
2020-08-26 17:19:29 -0500

    Waht

--- index.js
+++ index.js
@@ -48,6 +48,14 @@ app.use(session({
     }
 }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.use((req, res, next) => {
     if (req.cookies.user_sid && !req.session.user) {
         res.clearCookie('user_sid');        
